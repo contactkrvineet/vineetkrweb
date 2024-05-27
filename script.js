54d442d17a2c5744022b309532b843da8139b150
vineet kumar <contactkrvineet@gmail.com>
2024-05-27 14:37:25 -0400

    code added

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ document
 
     // Here, you could also add code to actually send the message to your email or backend server.
   });
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  var mailtoLink =
+    "mailto:contactkrvineet@gmail.com" +
+    "?subject=" +
+    encodeURIComponent("Contact Form Submission from " + name) +
+    "&body=" +
+    encodeURIComponent(
+      "Name: " + name + "\nEmail: " + email + "\nMessage: " + message
+    );
+
+  window.location.href = mailtoLink;
+}
