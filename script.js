db1a6b666942c8516a049affab0009bce7954f0d
vineet kumar <contactkrvineet@gmail.com>
2026-01-03 05:53:25 -0500

    Redesigned

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    alert(`Thank you, ${name}! Your message has been sent.`);
-
-    // Here, you could also add code to actually send the message to your email or backend server.
-  });
-
 function sendEmail(event) {
   event.preventDefault();
 
